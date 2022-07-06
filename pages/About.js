7f96793f4d6262374759db630e8fe59ca96be045
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 11:05:50 +0100

    some changes in global style

--- pages/About.js
+++ pages/About.js
@@ -1,15 +1,10 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-
 const About = () => {
     return (
       <div>
-        <Navbar/>
         <h1>About</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <Footer/>
       </div>
     );
   }
