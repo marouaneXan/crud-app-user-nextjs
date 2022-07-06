dd91f1875326720b7c18baa6550fc2a63f87f46d
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 12:12:04 +0100

    get all ninjas

--- pages/ninjas/Ninjas.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Ninjas = () => {
-  return (
-    <div>Ninjas Page</div>
-  )
-}
-
-export default Ninjas
---
+++ pages/ninjas/index.js
@@ -0,0 +1,24 @@
+import Ninjas from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { ninjas: data },
+  };
+};
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Ninjas;
