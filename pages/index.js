7b87d328c669e77a849c5bbbe0f096a53bbe7fb2
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 11:00:43 +0100

    add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolorum saepe aspernatur repellendus voluptas reprehenderit quam ab autem, aliquid officiis ullam deserunt beatae tempora, expedita perspiciatis, porro vitae obcaecati aut.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro explicabo error facilis officiis natus nihil qui, voluptatem consequatur dolor earum asperiores eos dolorem assumenda laborum mollitia fugit sunt nam? Enim!</p>
+      <Link href="/ninjas">
+        <a>Ninjas Listening</a>
+      </Link>
       <Footer/>
     </div>
   )
