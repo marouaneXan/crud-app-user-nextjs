36bdef864705b490340a4a45d0d3bd74426e0ed2
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 14:24:41 +0100

    fix some errors for getting details of users

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,13 +1,13 @@
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
-  const path = data.map((ninja) => {
+  const paths = data.map(ninja => {
     return {
-      params: { id: ninja.id.toString() },
+      params: { id: ninja.id.toString() }
     };
   });
   return {
-    path,
+    paths,
     fallback:false
   }
 }
@@ -17,17 +17,17 @@ export const getStaticProps=async (context)=>{
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`) 
   const data= await res.json()
   return {
-    params:{ninja:data}
+    props:{ninja:data}
   }
 }
-const Details = (ninja) => {
+const Details = ({ninja}) => {
   return (
     <div>
       <h1>User details</h1>
       <h2>{ninja.name}</h2>
       <p>{ninja.email}</p>
       <p>{ninja.website}</p>
-      <p>{ninja.adress.city}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
