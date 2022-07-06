d8f13b8d9cdb1091a07e8c0aea9cdb732ed04055
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 11:36:24 +0100

    finish styling for home page

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolorum saepe aspernatur repellendus voluptas reprehenderit quam ab autem, aliquid officiis ullam deserunt beatae tempora, expedita perspiciatis, porro vitae obcaecati aut.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro explicabo error facilis officiis natus nihil qui, voluptatem consequatur dolor earum asperiores eos dolorem assumenda laborum mollitia fugit sunt nam? Enim!</p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolorum saepe aspernatur repellendus voluptas reprehenderit quam ab autem, aliquid officiis ullam deserunt beatae tempora, expedita perspiciatis, porro vitae obcaecati aut.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro explicabo error facilis officiis natus nihil qui, voluptatem consequatur dolor earum asperiores eos dolorem assumenda laborum mollitia fugit sunt nam? Enim!</p>
       <Link href="/ninjas">
-        <a>Ninjas Listening</a>
+        <a  className={styles.btn}>Ninjas Listening</a>
       </Link>
     </div>
   )
