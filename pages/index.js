882d34b952ebb1458fa1a51d6c6e5ce77a9f5ade
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-06 11:56:38 +0100

    add head for home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolorum saepe aspernatur repellendus voluptas reprehenderit quam ab autem, aliquid officiis ullam deserunt beatae tempora, expedita perspiciatis, porro vitae obcaecati aut.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro explicabo error facilis officiis natus nihil qui, voluptatem consequatur dolor earum asperiores eos dolorem assumenda laborum mollitia fugit sunt nam? Enim!</p>
-      <Link href="/ninjas">
-        <a  className={styles.btn}>Ninjas Listening</a>
-      </Link>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Ninjas List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolorum
+          saepe aspernatur repellendus voluptas reprehenderit quam ab autem,
+          aliquid officiis ullam deserunt beatae tempora, expedita perspiciatis,
+          porro vitae obcaecati aut.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+          explicabo error facilis officiis natus nihil qui, voluptatem
+          consequatur dolor earum asperiores eos dolorem assumenda laborum
+          mollitia fugit sunt nam? Enim!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>Ninjas Listening</a>
+        </Link>
+      </div>
+    </>
+  );
 }
